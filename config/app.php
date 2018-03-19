f01fbf99913d751197a8670fc9335c0f49f6d3cf
Mark Guagenti <mark@guagenti.co>
2018-03-19 17:57:57 -0500

    Add markdown library

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        'Collective\Html\HtmlServiceProvider',
 
     ],
 
@@ -209,6 +210,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Uuid' => Webpatser\Uuid\Uuid::class,
+        'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
+        'Form' => 'Collective\Html\FormFacade',
+        'Html' => 'Collective\Html\HtmlFacade',
 
     ],
 
---
+++ resources/views/draft.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Draft post</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        {!! Form::open(['url' => '/admin/draft/']) !!}
+                            {!! Form::label('title', 'Post title'); !!}
+                            {!! Form::text('title', null, ['class' => 'form-control']); !!}
+
+                            {!! Form::label('slug', 'Post slug'); !!}
+                            {!! Form::text('slug', null, ['class' => 'form-control']); !!}
+
+                            {!! Form::label('post_date', 'Post date'); !!}
+                            {!! Form::text('post_date', null, ['class' => 'form-control']); !!}
+
+                            {!! Form::label('description', 'Post meta description'); !!}
+                            {!! Form::text('description', null, ['class' => 'form-control']); !!}
+
+                            {!! Form::label('content', 'Post body'); !!}
+                            {!! Form::textarea('content', null, ['class' => 'form-control']); !!}
+
+                            {!! Form::submit('Submit', ['class' => 'btn btn-lg btn-success form-control']) !!}
+
+                        {!! Form::close() !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
